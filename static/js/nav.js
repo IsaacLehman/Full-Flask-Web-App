f1ae005e64768714c2860459470bb46ccda513c6
Isaac Lehman <isaac.t.lehman@gmail.com>
2021-11-20 22:24:52 -0500

    Added blog single read depth reference

--- static/js/nav.js
+++ static/js/nav.js
@@ -20,3 +20,19 @@ if (window.scrollY > 40) {
     navbar.classList.remove('nav-active');
     navbar.classList.remove('bottom-shadow');
 }
+
+// for blog single pages - reading progress
+if (typeof isSingleBlog !== 'undefined') { // set in blog single template
+    const progressBar = document.querySelector("#progressBar");
+    let totalPageHeight = document.body.scrollHeight - window.innerHeight;
+    // do initial set
+    let newProgressHeight = (window.pageYOffset / totalPageHeight) * 100;
+    progressBar.style.width = `${newProgressHeight}%`;
+    progressBar.style.opacity = `${newProgressHeight}%`;
+    // watch for scroll
+    window.onscroll = () => {
+        newProgressHeight = (window.pageYOffset / totalPageHeight) * 100;
+        progressBar.style.width = `${newProgressHeight}%`;
+        progressBar.style.opacity = `${newProgressHeight}%`;
+    };
+}
